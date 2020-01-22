8eea19ee53ba79d83513ec76bc32fa296b4a63aa
vijay-gonuclei <vijay.kumar@gonuclei.com>
2020-01-22 07:49:11 +0530

    new lib files

---
+++ Example/Pods/Target Support Files/GLMediaCompressor/GLMediaCompressor-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GLMediaCompressor : NSObject
+@end
+@implementation PodsDummy_GLMediaCompressor
+@end
---
+++ Example/Pods/Target Support Files/Pods-GLMediaCompressor_Example/Pods-GLMediaCompressor_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GLMediaCompressor_Example : NSObject
+@end
+@implementation PodsDummy_Pods_GLMediaCompressor_Example
+@end
